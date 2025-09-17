81d6ad4af919d1b1fa675807b1fd035ee7203f01
Jordi <jordi.pastor@sky.uk>
2025-09-17 19:50:42 +0100

    JP code review

--- src/customer/CustomerForm.jsx
+++ src/customer/CustomerForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const CustomerForm = ({ setCustomers }) => {
   const [name, setName] = useState('');
-  const [email, seEmail] = useState('');
+  const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
 
@@ -20,7 +20,7 @@ const CustomerForm = ({ setCustomers }) => {
   return (
     <div className="form">
       <input type="text" placeholder="Name" onChange={value => setName(value.target.value)} value={name} />
-      <input type="email" placeholder="Email" onChange={value => seEmail(value.target.value)} value={email} />
+      <input type="email" placeholder="Email" onChange={value => setEmail(value.target.value)} value={email} />
       <input type="tel" placeholder="Phone" onChange={value => setPhone(value.target.value)} value={phone} />
       <input type="text" placeholder="Address" onChange={value => setAddress(value.target.value)} value={address} />
       <button onClick={() => submitEmployee()}>Add customer</button>
