210b4d2f6bf11197af38ff6f61027bfcfd81cc12
Jordi <jordi.pastor@sky.uk>
2025-09-18 14:46:44 +0100

    JP implicit return on customerList

--- src/customer/CustomerList.jsx
+++ src/customer/CustomerList.jsx
@@ -1,20 +1,17 @@
-const CustomerList = ({ customers }) => {
-  return (
-    <ul className="customer-list">
-      {
-        customers.map(customer => {
-          return (
-            <li key={customer.email}>
-              <strong>{customer.name}</strong><br />
-              <span>Email: {customer.email}</span><br />
-              <span>Phone: {customer.phone}</span><br />
-              <span>Address: {customer.address}</span><br />
-            </li>
-          )
-        })
-      }
-    </ul>
-  );
-};
+const CustomerList = ({ customers }) =>
+  <ul className="customer-list">
+    {
+      customers.map(customer => {
+        return (
+          <li key={customer.email}>
+            <strong>{customer.name}</strong><br />
+            <span>Email: {customer.email}</span><br />
+            <span>Phone: {customer.phone}</span><br />
+            <span>Address: {customer.address}</span><br />
+          </li>
+        )
+      })
+    }
+  </ul>;
 
 export default CustomerList;
