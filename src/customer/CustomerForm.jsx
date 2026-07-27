0b96f3b8d2f4276a744be7e5326b7e0a6b1521ef
jesus.sans <jesus.sans@sky.uk>
2026-07-27 14:48:46 +0200

    Fix typos

--- src/customer/CustomerForm.jsx
+++ src/customer/CustomerForm.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react';
 
 const CustomerForm = ({ setCustomers }) => {
   const [name, setName] = useState('');
-  const [email, seEmail] = useState('');
+  const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
 
   const clearInputs = () => {
     setName('');
-    seEmail('');
+    setEmail('');
     setPhone('');
     setAddress('');
   };
 
-  const submitEmployee = () => {
+  const submitCustomer = () => {
     // Solution Exercise 2
     // setCustomers(prev => {
     //   let newCustomer = [];
@@ -44,10 +44,10 @@ const CustomerForm = ({ setCustomers }) => {
   return (
     <div className="form">
       <input type="text" placeholder="Name" onChange={value => setName(value.target.value)} value={name} />
-      <input type="email" placeholder="Email" onChange={value => seEmail(value.target.value)} value={email} />
+      <input type="email" placeholder="Email" onChange={value => setEmail(value.target.value)} value={email} />
       <input type="tel" placeholder="Phone" onChange={value => setPhone(value.target.value)} value={phone} />
       <input type="text" placeholder="Address" onChange={value => setAddress(value.target.value)} value={address} />
-      <button onClick={() => submitEmployee()}>Add customer</button>
+      <button onClick={() => submitCustomer()}>Add customer</button>
       <button onClick={() => clearInputs()}>Clear</button>
     </div>
   );
